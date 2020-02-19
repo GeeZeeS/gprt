ff9f67e0bd544253bd42935480ee277604341ad4
denis <roundycube@gmail.com>
2020-02-19 15:21:22 +0200

    added mongo db to docker and implemented to app

--- services/web/project/__init__.py
+++ services/web/project/__init__.py
@@ -1,11 +1,18 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, send_from_directory
 from flask_sqlalchemy import SQLAlchemy
+from flask_pymongo import PyMongo
 
 app = Flask(__name__)
 app.config.from_object("project.config.Config")
 db = SQLAlchemy(app)
+mongo = PyMongo(app)
 
 
 @app.route("/")
 def hello_world():
     return jsonify(hello="world")
+
+
+@app.route("/static/<path:filename>")
+def staticfiles(filename):
+    return send_from_directory(app.config["STATIC_FOLDER"], filename)
