198ccb3d9b501195645c93f3f47bd421dd829e35
denis <roundycube@gmail.com>
2020-02-20 16:47:39 +0200

    added pandas to container

--- services/web/manage.py
+++ services/web/manage.py
@@ -1,7 +1,12 @@
 from flask.cli import FlaskGroup
-
+from flask import send_from_directory
 from project import app, db, mongo
 from project.models import Warehouse
+
+import os
+import json
+import re
+import pandas as pd
 
 
 cli = FlaskGroup(app)
@@ -16,7 +21,17 @@ def create_db():
 
 @cli.command("populate")
 def seed_mongo_db():
-    db = mongo.db.data
+    # mongo.db.tr
+    root_dir = app.config["STATIC_FOLDER"]
+    files = os.listdir(root_dir)
+    print(files)
+    for _file in files:
+        table_name = _file.split('_', 1)[0]
+        processing_file = open(os.path.join(root_dir, _file), 'r')
+        data = pd.read_csv(processing_file)
+        json_data = json.loads(data.to_json(orient='records'))
+        mongo.db.mng_db.remove()
+        mongo.db.mng_db[table_name].insert(json_data)
 
 
 if __name__ == "__main__":
