2c6a9b5ae7324fae17e3d6299dd9b4c7a9636301
denis <roundycube@gmail.com>
2020-02-25 16:44:11 +0200

    SQLALCHEMY_URI refactored

--- services/web/project/config.py
+++ services/web/project/config.py
@@ -10,15 +10,8 @@ DBPORT = '5432'
 DBNAME = 'pg_db'
 
 
-
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = \
-    'postgresql+psycopg2://{user}:{passwd}@{host}:{port}/{db}'.format(
-        user=DBUSER,
-        passwd=DBPASS,
-        host=DBHOST,
-        port=DBPORT,
-        db=DBNAME)
+    SQLALCHEMY_DATABASE_URI = f'postgresql+psycopg2://{DBUSER}:{DBPASS}@{DBHOST}:{DBPORT}/{DBNAME}'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     STATIC_FOLDER = f"{os.getenv('APP_FOLDER')}/project/static"
     MONGO_URI = MongoClient("mongodb://mongo:27017")
