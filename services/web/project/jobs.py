48fc0d8b8b054936f521b3c469f77ab44dd5c64d
denis <roundycube@gmail.com>
2020-02-27 09:29:56 +0200

    Code/comments refactoring

--- services/web/project/jobs.py
+++ services/web/project/jobs.py
@@ -5,9 +5,33 @@ from . import mongo, db, redis, scheduler, app
 from os import environ
 
 
-# write data to db
+def main_job():
+    whm_count = WarehouseModel.query.count()  # Get count of rows in DB
+    if whm_count > 0:  # Check if DB is populated
+        last_date = redis.get("last_date").decode('utf-8')  # Check if last_inserted date is stored
+        if last_date is None:
+            # Get last inserted date from db
+            whm_last_date = WarehouseModel.query.order_by(
+                WarehouseModel.created_at.desc()
+            ).first()
+            start_date = whm_last_date.created_at + timedelta(seconds=1)
+        else:
+            start_date = datetime.strptime(last_date, '%Y-%m-%d %H:%M:%S') + timedelta(seconds=1)
+        end_date = start_date + timedelta(seconds=299)
+    else:
+        # Getting lowest Object from list
+        orders_data_set = mongo.db.mng_db['orders'].find({}).sort([("created_at", 1)]).limit(1)
+
+        # Setting start and end dates
+        start_date = orders_data_set[0]['created_at']
+        end_date = datetime(2020, 1, 1)
+
+    print(f"Populating Database from {start_date} to {end_date}")  # Print info about populating data
+    populate_data(start_date, end_date)  # write data to db
+    redis.set("last_date", str(end_date).encode('utf-8'))  # set last_inserted date to redis
+
+
 def populate_data(start_date, end_date):
-    # Filter orders data between dates
     orders_data_set = mongo.db.mng_db['orders'].find(
         {
             "created_at": {
@@ -15,27 +39,22 @@ def populate_data(start_date, end_date):
                 "$lt": str(end_date)
             }
         }
-    ).sort([("created_at", 1)])
-    # Get Users Data
-    users_data_set = mongo.db.mng_db['users'].find({})
+    ).sort([("created_at", 1)])  # Filter and get orders
+
+    users_data_set = mongo.db.mng_db['users'].find({})  # Get Users
+
     # Dataframe Queries
     df_orders = DataFrame.from_records(orders_data_set)
     df_users = DataFrame.from_records(users_data_set)
-    # Check if Dataframe is empty
-    if not df_orders.empty:
-        # Merge Dataframes Together
-        df_final = merge(df_orders, df_users, on='user_id', how='left')
-        # Remove ObjectID Columns
-        df_final = df_final.drop(columns=['_id_x', '_id_y'])
-        # Rename Columns Based on the WarehouseModel model
-        df_final = column_rename(df_final)
-        # Write Dataframe to db
-        df_final.to_sql('warehouse', db.engine, if_exists='append', index=False)
-        # Output amount of rows inserted
-        print(f"{df_final.shape[0]} rows inserted")
+
+    if not df_orders.empty:  # Check if Dataframe is empty
+        df_final = merge(df_orders, df_users, on='user_id', how='left')  # Merge Dataframes Together
+        df_final = df_final.drop(columns=['_id_x', '_id_y'])  # Remove ObjectID Columns
+        df_final = column_rename(df_final)  # Rename Columns Based on the WarehouseModel model
+        df_final.to_sql('warehouse', db.engine, if_exists='append', index=False)  # Write Dataframe to db
+        print(f"{df_final.shape[0]} rows inserted")  # Output amount of rows inserted
     else:
-        # Output that no data was found within 5 minutes
-        print("Empty Query, Sleeping for the next 5 minutes")
+        print("Empty Query, Sleeping for the next 5 minutes")  # Output that no data was found to populate
 
 
 def column_rename(df):
@@ -52,52 +71,6 @@ def column_rename(df):
         }
     )
     return df
-
-
-def main_job():
-    # Get count of rows in DB
-    whm_count = WarehouseModel.query.count()
-    # Check if DB is populated
-    if whm_count > 0:
-        # If DB is populated check if last_inserted date is stored
-        last_date = redis.get("last_date").decode('utf-8')
-        if last_date is None:
-            # If last_inserted date is not stored,
-            # get last inserted date from db,
-            # and start from there
-            whm_last_date = WarehouseModel.query.order_by(
-                WarehouseModel.created_at.desc()
-            ).first()
-
-            # Setting start and end dates,
-            # adding 5 minutes to start_date
-            start_date = whm_last_date.created_at + timedelta(seconds=1)
-            end_date = start_date + timedelta(seconds=299)
-        else:
-            # If last_inserted date is stored,
-            # get date and add 5 minutes
-            start_date = datetime.strptime(last_date, '%Y-%m-%d %H:%M:%S') + timedelta(seconds=1)
-            end_date = start_date + timedelta(seconds=299)
-    else:
-        # Getting lowest Object from list
-        orders_data_set = mongo.db.mng_db['orders'].find({}).sort([("created_at", 1)]).limit(1)
-
-        # Setting start and end dates
-        start_date = orders_data_set[0]['created_at']
-        end_date = datetime(2020, 1, 1)
-    print_manager(start_date, end_date)
-    populate_data(start_date, end_date)
-    redis_manager(end_date)
-
-
-# set last_inserted date to redis
-def redis_manager(end_date):
-    redis.set("last_date", str(end_date).encode('utf-8'))
-
-
-# Print info about populating data
-def print_manager(start_date, end_date):
-    print(f"Populating Database from {start_date} to {end_date}")
 
 
 # Fix for scheduler to run once on startup
